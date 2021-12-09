72afc850bdc063edcbb195ba7f1c128dac036880
Sufiyan <75123948+sufiyankhangi@users.noreply.github.com>
2021-12-09 18:22:04 +0530

    Add files via upload

---
+++ Isprime.java
@@ -0,0 +1,27 @@
+import java.util.*;
+  
+  public class Isprime{
+  
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int t  = sc.nextInt();
+  
+       for(int i =0; i<t;i++){
+           int n = sc.nextInt();
+
+           int count = 0;
+           for(int div=2;div*div<=n;div++){
+               if (n%div == 0){
+                   count++;
+                   break;
+               }
+           }
+           if(count == 0){
+               System.out.println("prime no");
+           }else{
+               System.out.println("not prime");
+           }
+       }
+  
+   }
+  }
